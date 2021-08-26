c6e64538d8e2225df8b82e2b03c7f2bfa822d87d
Oleg Bloschinskyi <carrentkharkov@gmail.com>
2021-08-27 02:35:55 +0300

    27.08.21

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
+class CountdownTimer {
+  constructor({selector, targetDate}){
+    this.selector = selector;
+    this.targetDate = targetDate;
+    this.intervalId = null;
+  }
+  getRefs(){
+    const container = document.querySelector(this.selector);
+    const days = container.querySelector('[data-value = "days"]');
+    const hours = container.querySelector('[data-value="hours"]');
+    const minutes = container.querySelector('[data-value="min"]');
+    const seconds = container.querySelector('[data-value="secs"]');
+    const startBtn = container.querySelector('.start');
+    const stopBtn = container.querySelector('.stop');
+    return { container, days, hours, minutes, seconds, startBtn, stopBtn }
+  }
 
+  updateTimer({container, days, hours, minutes, seconds}){
+    const time = this.targetDate - Date.now();
 
-// class CountdownTimer {
-//   constructor({selector, targetDate}){
-//     this.selector = selector;
-//     this.targetDate = targetDate;
-//     this.intervalId = null;
-//   }
-//   getRefs(){
-//     const container = document.querySelector(this.selector);
-//     const days = container.querySelector('[data-value = "days"]');
-//     const hours = container.querySelector('[data-value="hours"]');
-//     const minutes = container.querySelector('[data-value="mins]');
-//     const seconds = container.querySelector('[data-value="secs"]');
-//     const startBtn = container.querySelector('.start');
-//     const stopBtn = container.querySelector('.stop');
-//     return { container, days, hours, minutes, seconds, startBtn, stopBtn }
-//   }
+    if(time < 0){
+      this.stop(container)
+      return
+    }
 
-//   updateTimer({container, days, hours, minutes, seconds}){
-//     const time = this.targetDate - Date.now();
-
-//     if(time < 0){
-//       this.stop(container)
-//       return
-//     }
-
-//     days.textContent = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
-//     hours.textContent = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
-//     minutes.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
-//     seconds.textContent = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, '0');
-//   }
+    days.textContent = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+    hours.textContent = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
+    minutes.textContent = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');;
+    seconds.textContent = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, '0');
+  }
   
-//   start(){
-//     console.log("start", this);
-//     this.intervalId = setInterval(() => {
-//      this.updateTimer(this.getRefs())
-//     }, 1000);
-//   }
+  start(){
+    console.log("start", this);
+    this.intervalId = setInterval(() => {
+     this.updateTimer(this.getRefs())
+    }, 1000);
+  }
 
-//   stop(container){
-//     clearInterval(this.intervalId)
-//     // container.innerHTML = "<h1>Время вышло</h1>"
+  stop(container){
+    clearInterval(this.intervalId)
+    container.innerHTML = "<h1>Время вышло</h1>"
     
-//   }
+  }
 
-//   addListeners({startBtn, stopBtn}){
-//     startBtn.addEventListener('click', this.start.bind(this));
-//      stopBtn.addEventListener('click', this.stop.bind(this));
-//   }
-// }
+  addListeners({startBtn, stopBtn}){
+    startBtn.addEventListener('click', this.start.bind(this));
+     stopBtn.addEventListener('click', this.stop.bind(this));
+  }
+}
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Aug 26, 2021 23:55'),
+  targetDate: new Date('Aug 28, 2021 04:54'),
 });
 
-// timer.addListeners(timer.getRefs());
+timer.addListeners(timer.getRefs());
